3fed1e6bbb9551673d97f8e7078a0ab019a9f634
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2025-07-08 22:30:46 +0530

    New component write

--- client/src/pages/owner/AddCar.jsx
+++ client/src/pages/owner/AddCar.jsx
@@ -123,6 +123,32 @@ const AddCar = () => {
             </div>
 
             {/* {Car Location} */}
+            <div className='flex flex-col w-full'>
+                <label>Location</label>
+                    <select onChange={e=> setCar({...car, location: e.target.value})} value=
+                    {car.location} className='px-3 py-2 mt-1 border border-borderColor
+                    rounded-md outline-none'>
+                        <option value="">Select Location</option>
+                        <option value="New York">New York</option>
+                        <option value="Los Angeles">Los Angeles</option>
+                        <option value="Houston">Houston</option>
+                        <option value="Chicago">Chicago</option>
+                    </select>
+            </div>
+            {/* {Car Description} */}
+            <div className='flex flex-col w-full'>
+                    <label>Description</label>
+                    <textarea rows={5} placeholder='e.g. A luxurious SUV with a specious interior and a powerful engine.' 
+                    required className='px-3 py-2 mt-1 border border-borderColor 
+                    rounded-md outline-none' value={car.description} 
+                    onChange={e=> setCar({...car, description:e.target.value})}></textarea>
+                </div>
+
+                <button className='flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary
+                text-white rounded-md font-medium w-max cursor-pointer'>
+                    <img src={assets.tick_icon} alt='' />
+                    List Your Car
+                </button>
 
         </form>
 
